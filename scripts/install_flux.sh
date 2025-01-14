b57d299ba6155544d3dc15520d6ccccbab0db9ba
tomas <tomas@kislan.sk>
2025-01-14 11:14:53 +0000

    Update flux

--- scripts/install_flux.sh
+++ scripts/install_flux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 rm -rf /tmp/flux && mkdir /tmp/flux && (cd /tmp/flux && \
-    curl -LO https://github.com/fluxcd/flux2/releases/download/v2.2.3/flux_2.2.3_linux_amd64.tar.gz && \
+    curl -LO https://github.com/fluxcd/flux2/releases/download/v2.4.0/flux_2.4.0_linux_amd64.tar.gz && \
     tar -xf flux_*_linux_amd64.tar.gz && \
     mv flux /usr/local/bin/flux && \
     flux --version) && \
